888c6f54d4836c924c3341b6a6598c513f229d34
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-04 22:49:51 -0300

    Using nodemoon

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello world'});
+    return response.json({message: 'Hello world!'});
 });
 
 app.listen(3333);

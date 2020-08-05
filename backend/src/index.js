af5a9d24ab70848fb58cb08e0d9283c7533215c1
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-04 22:56:35 -0300

    Http methods

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ * 
+ * GET -> Buscar informações do back-end.
+ * POST -> Criar alguma informação no back-end.
+ * PUT/PATCH -> Alterar alguma informação no back-end.
+ * DELETE -> Deletar alguma informação no back-end.
+ */
+
 app.get('/', (request, response) => {
     return response.json({message: 'Hello world!'});
 });

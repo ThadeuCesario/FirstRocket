3e0a5fd4b37c38d6c7506d3c8afa9f0fe11f4879
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-04 23:04:19 -0300

    Http methods

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,41 @@ const app = express();
  * POST -> Criar alguma informação no back-end.
  * PUT/PATCH -> Alterar alguma informação no back-end.
  * DELETE -> Deletar alguma informação no back-end.
+ * 
+ * Não há problema de utilizar rotas com o mesmo recurso porém com o métodos variando.
+ * Veja abaixo que tenho duas rotas com o recurso de projects. Porém os métodos são diferentes.
  */
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello world!'});
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
 });
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
 
 app.listen(3333, () => {
     console.log('Back-end started! 🚀');

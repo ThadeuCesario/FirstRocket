a3452a403693bbfa4776e61bd8d7937ba4e59ff5
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-05 22:46:00 -0300

    Finishing our api

--- backend/src/index.js
+++ backend/src/index.js
@@ -55,30 +55,44 @@ app.post('/projects', (request, response) => {
         owner,
     }
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    projects.push(project);
+
+    return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params;
+    const {id} = request.params;
+    const {title, owner} = request.body;
 
-    console.log(params);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'Project not found.'});
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const {id} = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'Project not found.'});
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 

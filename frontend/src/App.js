4ac2398378aed60f104cac5b67a3acec8a4ee8d7
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-08 22:19:22 -0300

    Main concepts of React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 import Header from './components/Header';
 
+/**
+ * Conceitos mais importantes do React
+ * 
+ * Componente
+ * Propriedade
+ * Estado
+ * 
+ */
+
 
 const App = _ => {
     return(

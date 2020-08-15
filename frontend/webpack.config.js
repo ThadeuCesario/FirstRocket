129d16c417ba5f64163f954dae1d845162b75e07
Thadeu Cesário <thadeucesario@FVFCVBBSP3XY.local>
2020-08-14 22:44:49 -0300

    loader css

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader' },
+                    { loader: 'css-loader' },
+                ]
             }
         ]
     }

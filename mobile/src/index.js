b82ed3210ec2bc54c85e4687a2311dbd7a0bbfc7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-08-30 18:36:35 -0300

    Listing project by an API
    Listing project by an API

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+import api from './services/api';
 
 /**
  * As tags dentro do React Native, não possuem valor semântico.
@@ -12,31 +13,55 @@ import {View, Text, StyleSheet, StatusBar} from 'react-native';
  * View: div, footer, header, main, aside, section
  * Text: p, span, strong, h1, h2, h3.
  * 
- * Podemos controlar nossa statusBar (a barra que fica no top), utilizando a importação dela. 
+ * Podemos controlar nossa statusBar (a barra que fica no top), utilizando a importação dela.
  * Pelo StatusBar e em seguida, aplicar diferentes estilos utilizando o barStyle.
+ * 
+ * Vamos utilizar o useEffect e também o useState para controlarmos os elementos.
+ * Lembre-se que o useEffect será disparado quando a variável dentro do array no segundo parâmetro for alterado.
+ * Se passarmos uma array vazio, será disparado somente uma vez, quando o elemento for carregado.
+ * 
+ * Lembre-se de utilizar FlatList ao invés de ScrollView! Porque a FlatList é muito mais performática.
+ * 
+ * SafeAreView => Todo o conteúdo será renderizado somente na área segura do mobile.
  */
 
-export default App = _ny => {
-  return(
+export default App = _ => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  return (
     <>
-      <StatusBar barStyle={'dark-content'}/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack.</Text>
-      </View>
+      <StatusBar barStyle={'dark-content'} />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => `${project.id}`}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159C1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   title: {
     color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 30,
   },
 });
